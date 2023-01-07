f1c1457b3ff0e74fe7e78be0bfff182db8bd132a
Hasler TEHOU <tehoumanoel96@gmail.com>
2023-01-07 10:00:11 +0100

    update

--- assets/js/octhama.js
+++ assets/js/octhama.js
@@ -91,7 +91,7 @@
     })
 
     /**
-     * Scrool with ofset on links with a class name .scrollto
+     * Scroll with offset on links with a class name .scrollto
      */
     on('click', '.scrollto', function(e) {
         if (select(this.hash)) {
@@ -203,35 +203,6 @@
             el: '.swiper-pagination',
             type: 'bullets',
             clickable: true
-        }
-    });
-
-    /**
-     * Testimonials slider
-     */
-    new Swiper('.testimonials-slider', {
-        speed: 600,
-        loop: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false
-        },
-        slidesPerView: 'auto',
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 20
-            },
-
-            1200: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            }
         }
     });
 
